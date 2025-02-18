171fbabef368ef673c81ba5979a3a1da12e4c731
Sunz1990 <narongsuk.d@gmail.com>
2025-02-18 21:57:24 +0700

    main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from pystyle import Colors, Colorate
 
 from Sunztool import Sunztool
 
-__CHANNEL_USERNAME__ = "Sunz1990Channel"
-__GROUP_USERNAME__   = "Sunz1990Chat"
+__CHANNEL_USERNAME__ = "SunztoolChannel"
+__GROUP_USERNAME__   = "SunztoolChat"
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
