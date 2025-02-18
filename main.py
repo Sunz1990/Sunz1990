54bfad7f1fe2acfe5b6c808221d6fa2d92222a40
Sunz1990 <narongsuk.d@gmail.com>
2025-02-18 21:55:27 +0700

    main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from Sunztool import Sunz1990
+from Sunztool import Sunztool
 
 __CHANNEL_USERNAME__ = "Sunz1990Channel"
 __GROUP_USERNAME__   = "Sunz1990Chat"
